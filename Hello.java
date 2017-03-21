632ebe7f3c13b5c4d85984de516fb9219adf7391
haoshou <2630388713@qq.com>
2017-03-21 23:31:26 +0800

    git commit message by idea

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ public class Hello {
     public static void main(String arg[]) {
         System.out.println("Hello World!master");
         System.out.print("test idea sucess!");
+        System.out.print("2017 test github commit by idea");
     }
 }

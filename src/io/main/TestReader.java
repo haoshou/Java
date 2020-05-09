f1a45facc42de3f0cb72ff8c97c8487f85514808
haoshou <2630388713@qq.com>
2020-05-09 22:31:41 +0800

    long time test commit

--- src/io/main/TestReader.java
+++ src/io/main/TestReader.java
@@ -1,20 +1,40 @@
 package io.main;
 /**
- *  Reader是进行字符数据读取的操作类
- *  Reader依然是抽象类，那么如果从文件读取，依然使用FileReader类。
+ * Reader是进行字符数据读取的操作类
+ * Reader依然是抽象类，那么如果从文件读取，依然使用FileReader类。
  */
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 public class TestReader {
-	public static void main(String[] args) throws IOException {
-		File file = new File("D:\\test.txt");
-		FileReader reader = new FileReader(file);
-		char data[] = new char[1024];
-		int read = reader.read(data);
-		System.out.println(new String(data,0,read));
-		reader.close();
-	}
+    public static void main(String[] args) throws IOException {
+        TestFileReader();
+        return;
+    }
+
+    public static void TestFileReader() throws IOException {
+        File file = new File("D:\\test.txt");
+        FileReader reader = new FileReader(file);
+        char data[] = new char[1024];
+        int read = reader.read(data);
+        System.out.println(new String(data, 0, read));
+        reader.close();
+    }
+
+
+    /**
+     * 实现逐行输出文本文件的内容
+     * @param filePath
+     * @throws IOException
+     */
+    public static void readFileContent(String filePath) throws IOException {
+        FileReader reader = new FileReader(filePath);
+        BufferedReader bufferedReader = new BufferedReader(reader);
+        String line;
+        while ((line = bufferedReader.readLine()) != null) {
+            System.out.println(line);
+        }
+        bufferedReader.close();
+    }
+
 }
